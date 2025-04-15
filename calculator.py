fc6c7536831b4ef1c4d80055e1ea0a7eebd64a69
LSKing28 <mhird@ufl.edu>
2025-04-14 22:17:59 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,35 +4,9 @@
 
 import math
 
-
-# First example
-def add(a, b):
-    return a + b
-def sub(a, b):
-    return a - b
-def mul(a, b):
-    return a * b
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return b / a
-    return b / a
-def log(a, b):
-    if a == 1:
-        raise ValueError("Base can't equal 1")
-    elif a < 0:
-        raise ValueError("Base must be greater than 0")
-    elif b < 0:
-        raise ValueError("Argument must be greater than 0")
-    return math.log(b, a)
-def exp(a, b):
-    return a ** b
-
-import math
-
 def square_root(a):
     if a < 0:
-        raise ValueError("Cannot take square root of a negative number.")
+        raise ValueError("Cannot compute square root of negative number.")
     return math.sqrt(a)
 
 def hypotenuse(a, b):
@@ -46,6 +20,11 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
 
 def logarithm(a, b):
     if a <= 0 or b <= 0 or a == 1:
